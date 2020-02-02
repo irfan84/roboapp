79f1db9587af4f842f68afeb9904b8f8d517030f
Irfan <mirfanshakeel84@gmail.com>
2020-02-02 22:38:57 +1300

    Updated filtered function and passed as prop to cardlist to search robots

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import robots from "./robots";
 import SearchBox from './SearchBox';
 import CardList from "./CardList";
 
-const App = () => {
 
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+    });
     return (
         <div className='tc'>
             <h1>Robo Friends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
         </div>
     );
+}
 }
 
 export default App;
